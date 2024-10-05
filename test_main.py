f077b4fba2a3796bbaaf1261e930ab91cc713540
Duify <grisha123berberov654@gmail.com>
2024-10-05 12:44:48 +0300

    Add requirements and update ci.yml

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 import sys
-from main import add, subtract
+from src.main import add, subtract
 
 # Добавляем путь к пакету src в системный путь Python
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
